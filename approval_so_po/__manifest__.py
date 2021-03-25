a64e4be5acb70162879b474331677c069a3b35d2
fazeelshahzad <49979617+fazeelshahzad@users.noreply.github.com>
2021-03-25 13:02:44 +0500

    Add files via upload

--- approval_so_po/__manifest__.py
+++ approval_so_po/__manifest__.py
@@ -23,7 +23,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/purchase_views.xml',
         'security/security.xml',
         'views/account_views.xml',
